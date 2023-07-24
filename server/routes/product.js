7f660e2e90574daee990b18af800d812e0e103ad
iamprincejkc <princejankevin@gmail.com>
2023-07-24 20:42:21 +0800

    Create api for search

--- server/routes/product.js
+++ server/routes/product.js
@@ -18,4 +18,15 @@ productRouter.get('/api/products', auth, async (req, res) => {
 });
 
 
+
+productRouter.get('/api/products/search:name', auth, async (req, res) => {
+    try {
+        const products = await Product.find({ name: { $regex: req.params.name, $options: "i" } });
+        res.json(products);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+
 module.exports = productRouter;
