f717f7b184d797f1d45027a2a693e765dae007ca
iamprincejkc <princejankevin@gmail.com>
2023-07-27 21:27:57 +0800

    Add Deals of Day API

--- server/routes/product.js
+++ server/routes/product.js
@@ -59,4 +59,25 @@ productRouter.post('/api/rate-product', auth, async (req, res) => {
 });
 
 
+productRouter.get('/api/deal-of-day', auth, async (req, res) => {
+    try {
+        let products = await Product.find({});
+        products.sort((product1, product2) => {
+            let product1Sum = 0;
+            let product2Sum = 0;
+            for (let i = 0; i < product1.ratings.length; i++) {
+                product1Sum += product1.ratings[index].rating;
+            }
+            for (let i = 0; i < product2.ratings.length; i++) {
+                product2Sum += product2.ratings[index].rating;
+            }
+            return product1 < product2 ? 1 : -1;
+        });
+        res.json(products[0]);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+
 module.exports = productRouter;
