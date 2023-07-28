b794dffe6ea94bb55c3000859bc1dca4a3248600
iamprincejkc <princejankevin@gmail.com>
2023-07-28 20:03:55 +0800

    Creatre add to cart method

--- lib/features/product_details/services/product_details_services.dart
+++ lib/features/product_details/services/product_details_services.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_pay/models/product.dart';
 import 'package:http/http.dart' as http;
+import 'package:shop_pay/models/user.dart';
 
 import '../../../constants/error_handling.dart';
 import '../../../constants/global_variables.dart';
@@ -11,6 +12,35 @@ import '../../../constants/utils.dart';
 import '../../../providers/user_provider.dart';
 
 class ProductDetailsServices {
+  void addToCart({
+    required BuildContext context,
+    required Product product,
+  }) async {
+    final userProvider = Provider.of<UserProvider>(context, listen: false);
+    try {
+      http.Response response = await http.post(
+        Uri.parse('$uri/api/add-to-cart'),
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+          'x-auth-token': userProvider.user.token,
+        },
+        body: jsonEncode({
+          'id': product.id,
+        }),
+      );
+      httpErrorHandle(
+          response: response,
+          context: context,
+          onSuccess: (() {
+            User user = userProvider.user
+                .copyWith(cart: jsonDecode(response.body)['cart']);
+            userProvider.setUserFromModel(user);
+          }));
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
+  }
+
   void rateProduct({
     required BuildContext context,
     required Product product,
