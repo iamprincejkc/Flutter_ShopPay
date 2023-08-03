04a428e490d065ea090889afb8946fac2f46daa9
iamprincejkc <princejankevin@gmail.com>
2023-08-03 21:47:16 +0800

    Create API Method for Get Orders

--- lib/features/admin/services/admin_services.dart
+++ lib/features/admin/services/admin_services.dart
@@ -5,6 +5,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_pay/constants/error_handling.dart';
 import 'package:shop_pay/constants/utils.dart';
+import 'package:shop_pay/models/order.dart';
 import 'package:shop_pay/models/product.dart';
 import 'package:http/http.dart' as http;
 import 'package:shop_pay/providers/user_provider.dart';
@@ -119,5 +120,37 @@ class AdminServices {
     } catch (e) {
       showSnackBar(context, e.toString());
     }
+  }
+
+  Future<List<Order>> fetchAllOrders(BuildContext context) async {
+    final userProvider = Provider.of<UserProvider>(context, listen: false);
+    List<Order> orderList = [];
+    try {
+      http.Response response = await http.get(
+        Uri.parse('$uri/admin/get-orders'),
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+          'x-auth-token': userProvider.user.token,
+        },
+      );
+      httpErrorHandle(
+        response: response,
+        context: context,
+        onSuccess: () {
+          for (int i = 0; i < jsonDecode(response.body).length; i++) {
+            orderList.add(
+              Order.fromJson(
+                jsonEncode(
+                  jsonDecode(response.body)[i],
+                ),
+              ),
+            );
+          }
+        },
+      );
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
+    return orderList; 
   }
 }
